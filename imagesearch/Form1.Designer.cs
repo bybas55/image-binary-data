ef6926f8af80d7f9768b59d69e8bc4e9ab1da449
Bayram Baş <bbas5534@hotmail.com>
2020-01-10 00:18:42 +0200

    project pertama

---
+++ imagesearch/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace imagesearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ıDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imagedataDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.imageBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new imagesearch.DataSet1();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageTableAdapter = new imagesearch.DataSet1TableAdapters.imageTableAdapter();
+            this.imageBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet2 = new imagesearch.DataSet2();
+            this.imageTableAdapter1 = new imagesearch.DataSet2TableAdapters.imageTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(229, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(167, 159);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "dosya seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "kaydetme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ıDDataGridViewTextBoxColumn,
+            this.imagedataDataGridViewImageColumn});
+            this.dataGridView1.DataSource = this.imageBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 252);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 169);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // ıDDataGridViewTextBoxColumn
+            // 
+            this.ıDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.ıDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.ıDDataGridViewTextBoxColumn.Name = "ıDDataGridViewTextBoxColumn";
+            this.ıDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // imagedataDataGridViewImageColumn
+            // 
+            this.imagedataDataGridViewImageColumn.DataPropertyName = "imagedata";
+            this.imagedataDataGridViewImageColumn.HeaderText = "imagedata";
+            this.imagedataDataGridViewImageColumn.Name = "imagedataDataGridViewImageColumn";
+            // 
+            // imageBindingSource
+            // 
+            this.imageBindingSource.DataMember = "image";
+            this.imageBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // imageTableAdapter
+            // 
+            this.imageTableAdapter.ClearBeforeFill = true;
+            // 
+            // imageBindingSource1
+            // 
+            this.imageBindingSource1.DataMember = "image";
+            this.imageBindingSource1.DataSource = this.dataSet2;
+            // 
+            // dataSet2
+            // 
+            this.dataSet2.DataSetName = "DataSet2";
+            this.dataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // imageTableAdapter1
+            // 
+            this.imageTableAdapter1.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 433);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource imageBindingSource;
+        private DataSet1TableAdapters.imageTableAdapter imageTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn imagedataDataGridViewImageColumn;
+        private DataSet2 dataSet2;
+        private System.Windows.Forms.BindingSource imageBindingSource1;
+        private DataSet2TableAdapters.imageTableAdapter imageTableAdapter1;
+    }
+}
+
---
+++ imagesearch/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace imagesearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //veritabanı bağlantısı burada yer almaktadır
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-0OLBCM7\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=DbNotKayit");
+
+        string resimpath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title="yüklenicek fotografı seçiniz...";
+            openFileDialog1.Filter = "Jpeg Dosyası (*.jpg)|.jpg|Png Dosyası (*.png)|.png";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.CheckFileExists = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                resimpath = openFileDialog1.FileName.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //resim yüklemek için filestream metodunu kullanıyoruz...
+            FileStream fs = new FileStream(resimpath,FileMode.Open,FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            byte[] resim = br.ReadBytes((int)fs.Length);
+            br.Close();
+            fs.Close();
+            //veritabanı
+            SqlCommand komut = new SqlCommand("insert into image(imagedata) values (@p1)",baglanti);
+            komut.Parameters.Add("@p1",SqlDbType.Image, resim.Length).Value = resim;
+            //hata almamak için try kullanıcağız
+            try
+            {
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                MessageBox.Show("veritabanına başarıyla kayıt edilmiştir");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+
+                baglanti.Close();
+            }
+            this.imageTableAdapter.Fill(this.dataSet1.image);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.image' table. You can move, or remove it, as needed.
+            this.imageTableAdapter.Fill(this.dataSet1.image);
+
+        }
+        string secilen2;
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            secilen2 = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * from image where ID='" + secilen2 + "'",baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                //binary olarak eklediğimiz image geri çevirerek dönüştürüp picturebox2'ye bastırdık
+                if (dr["imagedata"] != null)
+                {
+                    Byte[] data = new Byte[0];
+                    data = (Byte[])(dr["imagedata"]);
+                    MemoryStream mem = new MemoryStream(data);
+                    pictureBox2.Image = Image.FromStream(mem);
+                }
+                dr.Close();
+                komut.Dispose();
+                baglanti.Close();
+            }
+            this.imageTableAdapter.Fill(this.dataSet1.image);
+        }
+
+    }
+}
